7e1980a377a282074b2f667fc7318624f27233cc
Chris Berry <chrisberry222@hotmail.com>
2014-12-26 17:14:36 +0000

    Added null return for excep when loading html
    Also this build is not running at the time (like last one).

--- AnimalAdjectives/Code/AndroidSpecific/BitmapTools.cs
+++ AnimalAdjectives/Code/AndroidSpecific/BitmapTools.cs
@@ -29,7 +29,7 @@ namespace AnimalAdjectives.AndroidSpecific
 				Bitmap myBitmap = BitmapFactory.DecodeStream(input);
 				return myBitmap;
 			} catch (IOException) {
-				throw;
+				return null;
 			}
 		}
 	}
